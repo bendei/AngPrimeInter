223a119a927fa5d3b1d1ede7930c42d5abf2a48e
Bende István <istvan_bende@yahoo.com>
2021-07-22 18:29:28 +0200

    apróságok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -29,9 +29,7 @@ const routes: Routes = [
   ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],  
-  
-    
+    imports: [RouterModule.forRoot(routes)]
 })
 export class AppRoutingModule {
     
--- src/app/nyomonkovetes/main/mainview.component.ts
+++ src/app/nyomonkovetes/main/mainview.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainviewComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+
+    throw new Error("hehehehe");
+    
+   }
 
   ngOnInit(): void {
   }
